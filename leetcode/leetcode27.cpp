edbf7b24dc0687a80c4c7c327c4e1e73e2bd75d8
Nugget <anthony2205@sbcglobal.net>
2023-05-31 17:26:34 -0500

    Solution leetcode27

--- leetcode/leetcode27.cpp
+++ leetcode/leetcode27.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+/* Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
-using namespace std;
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
 
-int main(){
-    return 0;
-}
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+*/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] != val)
+                nums[k++] = nums[i];
+
+        return k;
+    }
+};
