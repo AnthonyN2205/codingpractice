863e72ee2da7ca141de25c6d1f06feaf438551bf
Nugget <anthony2205@sbcglobal.net>
2023-06-01 10:19:48 -0500

    made LC9 compilable

--- leetcode/leetcode9.cpp
+++ leetcode/leetcode9.cpp
@@ -1,5 +1,12 @@
 /* Given an integer x, return true if x is a palindrome, and false otherwise. */
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+
+int main(){
+    return 0;
+}
 class Solution {
 public:
     bool isPalindrome(int x) {
